83dccf191fb50d457628a6e873607bd332e9c907
Neha Rajpoot <neharajpoot233@gmail.com>
2024-07-09 16:39:38 +0530

    responsive contact page

--- Hamsasana/frontend/hamsasana/src/components/contact.jsx
+++ Hamsasana/frontend/hamsasana/src/components/contact.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import Lottie from 'lottie-react'
 
 
 import contact from '../../public/images/contact1.json'
 
 function Contact() {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_c3o0cf1', 'template_hekaw5c', form.current, {
+        publicKey: 'nHTLF_tusAg3_2TQr',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
     return (
         <>
-        <div className='bg-slate-100 dark:bg-slate-900 dark:text-white md:flex-col  md:grid-flow-col'>
+        <form ref={form} >
+        <div className='bg-slate-100  dark:bg-slate-900 dark:text-white max-w-screen  container  md:px-20  md:flex-col'>
         <h1 className='font-bold  text-left px-8 text-3xl mt-6 '>Contact Us</h1>
-            <div className='  flex flex-row  bg-slate-100 dark:bg-slate-900 dark:text-white mt-0'>
-            <div className='w-full m-28 mt-5  bg-slate-100 dark:bg-slate-900 dark:text-white grid grid-flow-col'>
+            <div className=' flex flex-col md:flex-row  bg-slate-100 dark:bg-slate-900 dark:text-white mt-0 justify-center'>
+            <div className=' bg-slate-100 dark:bg-slate-900 dark:text-white   md:flex-row  w-full  md:w-1/2 md:mt-10 mt-12  justify-center grid grid-flow-col'>
             
-                 <Lottie  loop={true} autoplay={true} animationData={contact} />
+              <Lottie  loop={true} autoplay={true} animationData={contact} />
             </div>
-                <div className='w-1/4 m-40 mt-5 space-y-2 bg-slate-100 dark:bg-slate-900 dark:text-white'>
+                <div className='w-full md:w-1/2 grid grid-flow-row justify-center  mt-5 space-y-2 bg-slate-100 dark:bg-slate-900 dark:text-white '>
                     <h3 className='font-bold text-xl'>Email</h3>
                     <label className="input input-bordered flex items-center gap-2 bg-white dark:bg-slate-800 dark:text-white text-black">
                     <svg
@@ -28,7 +48,7 @@ function Contact() {
                     d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                 </svg>
                 
-                    <input type="text" className="grow" placeholder="Email" />
+                    <input type="text" className="grow" placeholder="Email" name='from_email' />
                     </label>
                     <h3 className='font-bold text-xl'>Username:</h3>
                     <label className="input input-bordered flex items-center gap-2 bg-white dark:bg-slate-800 dark:text-white text-black">
@@ -40,20 +60,21 @@ function Contact() {
                     <path
                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                     </svg>
-                    <input type="text" className="grow" placeholder="Username" />
+                    <input type="text" className="grow" placeholder="Username" name='from_name' />
                     </label>
                     <h3 className='font-bold text-xl'>Message: </h3>
                     <br/>
                     <label>
                     <textarea name="message" id="messaage" cols="50" rows="10" className='bg-white rounded-md dark:bg-slate-800 dark:text-white text-black ' placeholder='Write your Message...' ></textarea>
                     </label>
-                    <button className="btn btn-xs border-none bg-orange-500 text-white hover:bg-orange-700 cursor-pointer hover:-translate-y-1 hover:scale-110 duration:300s sm:btn-sm md:btn-md lg:btn-lg">Submit</button>
+                    <button type='submit' className="btn btn-xs border-none bg-orange-500 text-white hover:bg-orange-700 cursor-pointer hover:-translate-y-1 hover:scale-110 duration:300s sm:btn-sm md:btn-md lg:btn-lg" ref={form} onSubmit={sendEmail}>Submit</button>
                     
                 
                 </div>
             </div>
             
         </div>
+        </form>
         </>
     )
 }
